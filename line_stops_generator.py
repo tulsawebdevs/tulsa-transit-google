b7ca13b2b4ad001b187108fe08398a8289b81435
groovecoder <luke.crouch@gmail.com>
2011-07-22 22:34:03 -0500

    pass dbf file to generate_line_stops

--- line_stops_generator.py
+++ line_stops_generator.py
@@ -8,12 +8,12 @@ def to_unicode(str):
     return str.decode('iso_8859_1')
 
 
-def generate_line_stops():
+def generate_line_stops(dbf_file):
     out = sqlite3.connect('line_stops.db')
     if not out:
         raise Exception('Unable to open sqlite3 database at %s' % output_db)
     out.text_factory = to_unicode
-    db_f = dbfpy.dbf.Dbf('stopsByLine.dbf')
+    db_f = dbfpy.dbf.Dbf(dbf_file)
 
     sql_drop = 'DROP TABLE IF EXISTS line_stops;'
     sql_create = 'CREATE TABLE line_stops (stop_id int, stop_abbr text, line_no text, line_dir text);'
@@ -29,8 +29,6 @@ def generate_line_stops():
         line_dir = linedir_str[-1]
         stop_id = row['StopId']
         stop_abbr = row['StopAbbr']
-        print(sql_insert)
-        print(stop_id, stop_abbr, line_no, line_dir)
         cur.execute(sql_insert, (stop_id, stop_abbr, line_no, line_dir))
 
     cur.close()
