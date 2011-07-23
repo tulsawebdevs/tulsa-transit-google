94ad2be05942b3db9f0ac6e1cc0d1271770d3961
John Whitlock <John-Whitlock@ieee.org>
2011-07-22 22:29:40 -0500

    Output Google Transit stop_times.txt and trips.txt

--- trip_parser.py
+++ trip_parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 '''Parse a trapeze text file'''
 
+import csv
 import re
 
 def parse_trapeze_text(text):
@@ -14,10 +15,11 @@ def parse_trapeze_stop_trips(text):
     '''Parse a trapeze Stop Trips file'''
     in_header = False
     header_done = False
-    in_direction = False
+    in_dir = False
+    dir_num = 0
     dir_headers = None
-    dir_rows = None
-    d = dict(meta=dict(),direction=dict())
+    dir_trips = None
+    d = dict(meta=dict(),dir=list())
     for linenum, line in enumerate(text.split('\n')):
         if in_header:
             if ':' in line:
@@ -28,32 +30,39 @@ def parse_trapeze_stop_trips(text):
                 in_header = False
             else:
                 raise ValueError('Header failure at line ' + linenum + '\n' + line)
-        elif in_direction:
+        elif in_dir:
             if line == '':
                 if dir_headers is None: dir_headers = []
-                elif dir_rows is None: dir_rows = []
+                elif dir_trips is None: dir_trips = []
                 else:
-                    d['direction'][in_direction]['rows'] = dir_rows
-                    in_direction = False
-            elif dir_rows is None:
+                    d['dir'][-1]['trips'] = dir_trips
+                    in_dir = False
+                    dir_num += 1
+            elif dir_trips is None:
                 if line.startswith('~'):
                     pattern = '^(' + line.replace('~','.').\
                         replace(' .',' (.').replace('. ','.) ')
+                    
                     if pattern[-1] == '.': pattern += ')'
                     dir_re = re.compile(pattern)
-                    d['direction'][in_direction]['headers'] = list()
                     headers = list()
                     for h in dir_headers:
                         headers.append(
                             [x.strip() for x in dir_re.match(h).groups()])
+                    minimized_headers = list()
                     for h in zip(*headers):
-                        d['direction'][in_direction]['headers'].append(
+                        minimized_headers.append(
                             [x for x in h if x])
+                    assert(minimized_headers[0] == ['Pattern'])
+                    assert(minimized_headers[1] == ['INT'])
+                    assert(minimized_headers[2] == ['VAL'])
+                    final_headers = minimized_headers[3:]
+                    d['dir'][-1]['headers'] = final_headers
                 else:
                     dir_headers.append(line)
             else:
-                dir_rows.append(
-                    [x.strip() for x in dir_re.match(line).groups()])
+                dir_trips.append(
+                    [x.strip() for x in dir_re.match(line).groups()[3:]])
             
         else:
             if linenum == 0: assert(line == 'Stop Trips')
@@ -65,15 +74,90 @@ def parse_trapeze_stop_trips(text):
                 name, raw_val = line.split(':',1)
                 val = raw_val.strip()
                 assert(val in ('To Downtown', 'From Downtown'))
-                d['direction'][val] = dict()
-                in_direction = val
+                d['dir'].append(dict(headers=list()))
+                d['dir'][-1]['name'] = val
+                in_dir = True
                 dir_headers = None
-                dir_rows = None
+                dir_trips = None
             elif line == '': continue
             else:
                 raise ValueError('I have no idea where I am at line ' + linenum + '\n' + line)
             
     return d
+
+
+def stop_id_lookup(stop_abbr, line_id, line_dir):
+    '''Find a stop ID, or return None'''
+    return stop_abbr
+
+def combine_tables(stop_data, routes_data):
+    
+    routes = dict([(b,a) for a,b,_,_ in routes_data])
+    route_id = routes[stop_data['meta']['Line']]
+    stop_data['meta']['route_id'] = route_id
+    service_id = stop_data['meta']['Service']
+
+    for dir_num, d in enumerate(stop_data['dir']):
+        d['headers'].insert(0, 'trip_id')
+        d['stop_ids'] = ['trip_id']
+        for t_num, t in enumerate(d['trips']):
+            trip_id = "%s_%s_%d_%02d" % (route_id, service_id, dir_num, t_num)
+            t.insert(0, trip_id)
+        for stop_abbrs in d['headers'][1:]:
+            stop_id = None
+            for stop_abbr in stop_abbrs:
+                stop_id = stop_id_lookup(stop_abbr, route_id, service_id)
+                if stop_id: break
+            d['stop_ids'].append(stop_id)
+
+
+def trip_columns():
+    return [
+        'route_id',         # from routes.txt
+        'service_id',       # from calendar.txt
+        'trip_id',          # primary key
+        'trip_headsign',    # "to Downtown", "Northbound"
+        'direction_id',     # 0 (outbound?) or 1 (inbound?)
+    ]
+
+def trips_from_data(stop_data):
+
+    route_id = stop_data['meta']['route_id']
+    service_id = stop_data['meta']['Service']
+    trips = list()
+    for dir_num, d in enumerate(data['dir']):
+        headsign = d['name']
+        for t_num, t in enumerate(d['trips']):
+            trip_id = t[0]
+            trips.append((route_id, service_id, trip_id, headsign, dir_num))
+    return trips
+
+def stop_columns():
+    return [
+        'trip_id',          # from trips.txt 
+        'arrival_time',     # - HH:MM:SS - 24hr
+        'departure_time',   # - HH:MM:SS - 24hr
+        'stop_id',          # from stops.txt
+        'stop_sequence',    # order of the stops for a trip - starts with 1
+    ]
+
+def stop_times_from_data(stop_data):
+
+    route_id = stop_data['meta']['route_id']
+    service_id = stop_data['meta']['Service']
+    stop_times = list()
+    for dir_num, d in enumerate(stop_data['dir']):
+        for t_num, t in enumerate(d['trips']):
+            trip_id = t[0]
+            for s_num, raw_time in enumerate(t[1:]):
+                # Trapeze uses '+' for approximate times (we think)
+                raw_time = raw_time.replace('+','')
+                hour, minute = [int(x) for x in raw_time.split(':')]
+                gtime = "%02d:%02d:00" % (hour, minute)
+                
+                stop_id = d['stop_ids'][s_num+1]
+                stop_times.append((trip_id, gtime, gtime, stop_id, s_num + 1))
+    return stop_times
 
 # From Stoptrips/100_1.txt
 st_100_1 = '''\
@@ -140,7 +224,29 @@ Pattern  INT  VAL    DAS1                                                       
 
 '''
 
+#route_id,route_short_name,route_long_name,route_type
+routes_data = [
+    ('2398','100','Admiral','3')
+]
+
 if __name__ == '__main__':
     data = parse_trapeze_text(st_100_1)
-    import pprint
-    pprint.pprint(data)
+    data2 = combine_tables(data, routes_data)
+    trips = [trip_columns()]
+    trips.extend(trips_from_data(data))
+    stops = [stop_columns()]
+    stops.extend(stop_times_from_data(data))
+    
+    destination_folder = "./"
+    output_name = 'trips'
+    with open('%s%s.txt' % (destination_folder,
+                            output_name), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(trips)
+        
+    output_name = 'stop_times'
+    with open('%s%s.txt' % (destination_folder,
+                            output_name), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(stops)
+    print "Done"
