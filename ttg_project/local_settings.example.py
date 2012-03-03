5985fc7c4d7c969e9912811ae38abf19dcd9ffce
John Whitlock <John-Whitlock@ieee.org>
2012-03-03 11:18:50 -0600

    More work on the ttg project

--- ttg_project/local_settings.example.py
+++ ttg_project/local_settings.example.py
@@ -1,4 +1,4 @@
-# Copy to local_settings.py and modify
+# Copy to local_settings.py and modify as desired
 
 import os.path
 ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -18,3 +18,12 @@ DATABASES = {
         'NAME': path('ttg.sqlite'),       # create in this folder
     }
 }
+
+LOCAL_INSTALLED_APPS = []
+# Add django-extensions manage.py commands, like runserver_plus
+try:
+    import django_extensions
+except ImportError:
+    pass
+else:
+    LOCAL_INSTALLED_APPS.append('django_extensions')
--- ttg_project/ttg/views.py
+++ ttg_project/ttg/views.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.contrib import messages
+from django.core.cache import cache
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import transitfeed
 
 from forms import UploadFileForm
 from models import MediaFile
@@ -24,7 +27,21 @@ def feed_zip(request, version):
 
 def viewer(request, version):
     '''View for the schedule viewer'''
-    return HttpResponse('Not implemented yet')
+    current_feed = MediaFile.objects.filter(file_type='F').latest('added_at')
+    schedule_key = 'schedule.%d' % current_feed.id 
+    schedule = cache.get(schedule_key)
+    if not schedule:    
+        schedule = transitfeed.Schedule(
+            problem_reporter=transitfeed.ProblemReporter())
+        current_feed = MediaFile.objects.filter(
+            file_type='F').latest('added_at')
+        schedule.Load(current_feed.abspath())
+        cache.set(schedule_key, schedule)
+
+    return render_to_response(
+        'ttg/viewer.html',
+        {},
+        RequestContext(request))
 
 
 def file_list(request):
