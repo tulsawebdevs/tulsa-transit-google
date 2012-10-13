362d4b95ee74f3bcad7b6f8750b614a0288278bb
jsatt <jsatt22@gmail.com>
2012-10-13 16:09:50 -0500

    add is_zipfile function for pre2.7 systems

--- ttg/forms.py
+++ ttg/forms.py
@@ -1,9 +1,39 @@
-from zipfile import is_zipfile
-
 from django import forms
 
 from mtta.tasks import import_mtta_signup
 from ttg.models import MediaFile
+
+import sys
+if sys.version_info >= (2, 7, 0):
+    from zipfile import is_zipfile
+else:
+    from zipfile import _EndRecData
+
+    
+    def _check_zipfile(fp):
+        try:
+            if _EndRecData(fp):
+                return True         # file has correct magic number
+        except IOError:
+            pass
+        return False
+
+
+    def is_zipfile(filename):
+        """Quickly see if a file is a ZIP file by checking the magic number.
+
+        The filename argument may be a file or file-like object too.
+        """
+        result = False
+        try:
+            if hasattr(filename, "read"):
+                result = _check_zipfile(fp=filename)
+            else:
+                with open(filename, "rb") as fp:
+                    result = _check_zipfile(fp)
+        except IOError:
+            pass
+        return result
 
 
 class UploadFileForm(forms.ModelForm):
