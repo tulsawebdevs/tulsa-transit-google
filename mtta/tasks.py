1395951cdf4d86da1e8202f2f12ebc38aa287a14
jsatt <jsatt22@gmail.com>
2012-10-13 16:54:28 -0500

    remove assertion

--- mtta/tasks.py
+++ mtta/tasks.py
@@ -13,7 +13,6 @@ from ttg.models import MediaFile
 def import_mtta_signup(media_file_id):
     media_file = MediaFile.objects.get(id=media_file_id)
 
-    assert(is_zipfile(media_file.file))
     signup = SignUp.objects.create(name='Imported at %s' % timezone.now())
     signup.import_zip(media_file.file)
     feed = signup.copy_to_feed()
