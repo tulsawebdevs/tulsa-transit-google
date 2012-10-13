dd722ea74c3d1d7f653de182761175e4cef5008c
jsatt <jsatt22@gmail.com>
2012-10-13 14:02:27 -0500

    fix import errors and pep8

--- mtta/tasks.py
+++ mtta/tasks.py
@@ -1,6 +1,8 @@
 from zipfile import is_zipfile
+import StringIO
 
 from celery.decorators import task
+from django.core.files.base import ContentFile
 from django.utils import timezone
 
 from mtta.models import SignUp
@@ -16,9 +18,9 @@ def import_mtta_signup(media_file_id):
     signup.import_zip(media_file.file)
     feed = signup.copy_to_feed()
     gtfs_str = StringIO.StringIO()
-    feed.export_gtfs(gtfs_file)
+    feed.export_gtfs(gtfs_str)
     gtfs_file = ContentFile(gtfs_str.getvalue())
     gtfs = MediaFile.objects.create(
         file_type=MediaFile.GTFS_FILE, source=MediaFile.GENERATED)
-    filename = 'gtfs_%s' % timezone.now().strftime('%Y-%m-%d')
+    filename = 'gtfs_%s.zip' % timezone.now().strftime('%Y-%m-%d')
     gtfs.file.save(filename, gtfs_file)
