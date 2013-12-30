e98bf8648c19767a5e2916301f0aeb87b3b2f266
John Whitlock <John-Whitlock@ieee.org>
2013-12-30 09:59:18 -0600

    Fix qa_check (nowhere near 100%)

--- qa_check.sh
+++ qa_check.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-./manage.py test --with-coverage
-if [[ $? -ne 0 ]]; then exit 1; fi
+TMP=`mktemp -t godo_coverage`
+coverage erase
+coverage run --branch ./manage.py test
+coverage report > $TMP
+cat $TMP
+if ! tail -n1 $TMP | grep -q "100%"; then
+    coverage html
+    open htmlcov/index.html
+fi
+rm $TMP
 flake8 .
